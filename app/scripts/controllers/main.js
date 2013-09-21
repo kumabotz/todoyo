40fe890668adc178df5f15e3be436068fa6bea30
kumabotz <kumabotz@gmail.com>
2013-09-21 18:41:02 +0800

    Enable local storage service

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,18 +1,15 @@
 'use strict';
 
 angular.module('mytodoApp')
-  // .controller('MainCtrl', function ($scope, localStorageService) {
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, localStorageService) {
 
     $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
 
-/*
     var todosInStore = localStorageService.get('todos');
     $scope.todos = todosInStore && todosInStore.split('\n') || [];
     $scope.$watch(function(){
       localStorageService.add('todos', $scope.todos.join('\n'));
     });
-*/
 
     $scope.addTodo = function(){
       $scope.todos.push($scope.todo);
