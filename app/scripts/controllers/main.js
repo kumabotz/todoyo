c06a125cf8923058ac9afc1b49c1eae9d9fcaeb6
kumabotz <kumabotz@gmail.com>
2013-09-21 14:23:19 +0800

    Disable angular-local-storage usage and fix function semi-colons

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,16 +1,25 @@
 'use strict';
 
 angular.module('mytodoApp')
+  // .controller('MainCtrl', function ($scope, localStorageService) {
   .controller('MainCtrl', function ($scope) {
 
     $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
 
+/*
+    var todosInStore = localStorageService.get('todos');
+    $scope.todos = todosInStore && todosInStore.split('\n') || [];
+    $scope.$watch(function(){
+      localStorageService.add('todos', $scope.todos.join('\n'));
+    });
+*/
+
     $scope.addTodo = function(){
       $scope.todos.push($scope.todo);
       $scope.todo = '';
-    }
+    };
 
     $scope.removeTodo = function(index){
       $scope.todos.splice(index, 1);
-    }
+    };
   });
