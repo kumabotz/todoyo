48cfae7fee8f259818f8b5e1324c0c3a4f98d3af
kumabotz <kumabotz@gmail.com>
2013-09-21 18:43:47 +0800

    Remove todo default items

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,6 @@
 
 angular.module('mytodoApp')
   .controller('MainCtrl', function ($scope, localStorageService) {
-
-    $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
-
     var todosInStore = localStorageService.get('todos');
     $scope.todos = todosInStore && todosInStore.split('\n') || [];
     $scope.$watch(function(){
